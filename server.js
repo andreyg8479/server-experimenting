790ccf30bf3111e716dcca0a0f79eabce5359c56
andreyg8479 <andreyg8479@gmail.com>
2026-01-03 19:06:54 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,49 @@
+const WebSocket = require("ws");
+
+// Use host-assigned port, or fallback to 8080 locally
+const PORT = process.env.PORT || 8080;
+
+const wss = new WebSocket.Server({ port: PORT });
+
+console.log(`Server running at ws://localhost:${PORT}`);
+
+
+
+
+messages = " \n \n \n \n \n \n \n \n Old Test \n Test";
+
+// Runs when a browser connects
+wss.on("connection", (socket) => {
+  console.log("Client connected");
+  
+  socket.send(messages);
+
+  // Runs when this client sends a message
+  socket.on("message", (message) => {
+    console.log("Received:", message.toString());
+	
+	string = message.toString();
+	
+	//remove all characters up to first newline
+	const firstIndex = messages.indexOf("\n")
+	messages = messages.substr(firstIndex + 1);
+	
+	
+	//add new message
+	messages = messages + "\n" + string;
+	
+	
+
+	//Sends to everyone
+    for (const client of wss.clients) {
+	  if (client.readyState === WebSocket.OPEN) { // make sure the client is still connected
+		client.send(messages);
+	  }
+	}
+
+  });
+
+  socket.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
